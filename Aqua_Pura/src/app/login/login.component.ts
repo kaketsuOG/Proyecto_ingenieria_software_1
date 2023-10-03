6478b7d0eb2e5c74dc49203c7c4eb7f6c68117ea
GeoC <diego.cubilhos@gmail.com>
2023-10-03 00:50:28 -0300

    Conexion lista

--- Aqua_Pura/src/app/login/login.component.ts
+++ Aqua_Pura/src/app/login/login.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  usuario: string = '';
+  contraseña: string = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
+  iniciarSesion() {
+    const url = 'http://localhost:3000/login'; // Cambia la URL según la configuración de tu servidor Node.js
+
+    this.http.post(url, { usuario: this.usuario, contraseña: this.contraseña }).subscribe(
+      (response: any) => {
+        console.log(response);
+        // Redirecciona o realiza acciones después del inicio de sesión exitoso
+      },
+      (error: any) => {
+        console.error(error);
+        // Muestra mensajes de error al usuario
+      }
+    );
   }
-
 }
