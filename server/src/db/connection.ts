8db6476fe1d739afae41fc40bad6a7d230a7a79d
kaketsuOG <seba.espinoza.f@gmail.com>
2023-10-16 16:41:33 -0300

    añadir carpeta frontend

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('hola', 'root', 'admin', {   // esto se edita segun como tengan nombrado su esquema, usuario y contraseña en su base de datos
+const sequelize = new Sequelize('aqua', 'root', 'admin', {   // esto se edita segun como tengan nombrado su esquema, usuario y contraseña en su base de datos
     host: 'localhost',
     dialect: 'mysql',
 });
