1c33be9e2850015d9d6a916e9690f4347621a0ad
juanpa511 <146480439+juanpa511@users.noreply.github.com>
2023-10-19 23:33:15 -0300

    Add files via upload

---
+++ server/src/models/det_horario_entrega.ts
@@ -0,0 +1,16 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../db/connection';
+
+export const Detalle_horario_entrega = sequelize.define('DETALLE_HORARIO_ENTREGAS',{
+    "COD_HORARIO_ENTREGA":{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+    },
+    "HORA_ENTREGA":{
+        type: DataTypes.TIME
+    }
+},
+{
+    freezeTableName: true,
+    timestamps: false,
+});
