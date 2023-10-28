c941b7e8c1213271d11f1816ccbf3644e153a6c7
NicolasPavz <npavez43@gmail.com>
2023-10-28 13:02:07 -0300

    mandar contraseña

--- server/dist/models/server.js
+++ server/dist/models/server.js
@@ -17,11 +17,11 @@ const cors_1 = __importDefault(require("cors"));
 const roles_1 = __importDefault(require("../routes/roles"));
 const user_1 = __importDefault(require("../routes/user"));
 const inventario_1 = __importDefault(require("../routes/inventario"));
-const productos_1 = __importDefault(require("../routes/productos"));
+const producto_1 = __importDefault(require("../routes/producto"));
 const rol_1 = require("./rol");
 const user_2 = require("./user");
 const inventario_2 = require("./inventario");
-const producto_1 = require("./producto");
+const producto_2 = require("./producto");
 class Server {
     constructor() {
         this.app = (0, express_1.default)();
@@ -33,14 +33,14 @@ class Server {
     }
     listen() {
         this.app.listen(this.port, () => {
-            console.log('Corriendo en el puerto ' + this.port);
+            console.log('Corriendo en el puertoo ' + this.port);
         });
     }
     routes() {
         this.app.use('/api/roles', roles_1.default);
         this.app.use('/api/users', user_1.default);
         this.app.use('/api/inventraio', inventario_1.default);
-        this.app.use('/api/productos', productos_1.default);
+        this.app.use('/api/productos', producto_1.default);
     }
     midlewares() {
         //parseo body
@@ -52,12 +52,12 @@ class Server {
         return __awaiter(this, void 0, void 0, function* () {
             try {
                 yield inventario_2.Inventario.sync();
-                yield producto_1.Producto.sync();
+                yield producto_2.Producto.sync();
                 yield user_2.User.sync();
                 yield rol_1.Rol.sync();
             }
             catch (error) {
-                console.error('No se ha podido conectar a la base de datoos');
+                console.error('No se ha podido conectar a la base de datos');
             }
         });
     }
