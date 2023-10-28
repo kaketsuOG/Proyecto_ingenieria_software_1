ead3d37394effa90c31dcfef2b570b0d4dcb011a
GeoC6 <120148441+GeoC6@users.noreply.github.com>
2023-10-28 20:37:13 -0300

    Actualizacion de server.ts para la aplicacion de Vehiculo

--- server/src/models/server.ts
+++ server/src/models/server.ts
@@ -2,12 +2,14 @@ import express, {Application} from 'express';
 import cors from 'cors';
 import routesRoles from '../routes/roles';
 import routesUser from '../routes/user';
-import routesInventario from '../routes/inventario'
-import routesProducto from '../routes/producto'
+import routesInventario from '../routes/inventario';
+import routesProducto from '../routes/producto';
+import routesVehiculo from '../routes/vehiculo';
 import { Rol } from './rol';
 import { User } from './user';
 import { Inventario } from './inventario';
 import { Producto } from './producto';
+import { Vehiculo } from './vehiculo';
 
 class Server {
     private app: Application;
@@ -34,6 +36,7 @@ class Server {
         this.app.use('/api/users', routesUser);
         this.app.use('/api/inventraio',routesInventario);
         this.app.use('/api/productos', routesProducto);
+        this.app.use('/api/vehiculos', routesVehiculo);
     }
 
     midlewares() {
@@ -50,6 +53,7 @@ class Server {
             await Producto.sync()
             await User.sync()
             await Rol.sync()
+            await Vehiculo.sync()
         }catch (error){
             console.error('No se ha podido conectar a la base de datos');
         }
