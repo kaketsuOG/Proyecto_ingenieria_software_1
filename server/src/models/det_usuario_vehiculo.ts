c4a1c24508f2432f71b7b665d305760e77c9026e
GeoC6 <120148441+GeoC6@users.noreply.github.com>
2023-11-07 00:18:47 -0400

    Update det_usuario_vehiculo.ts

--- server/src/models/det_usuario_vehiculo.ts
+++ server/src/models/det_usuario_vehiculo.ts
@@ -4,14 +4,13 @@ import { User } from './user';
 import { Vehiculo } from './vehiculo';
 
 export const Det_usuario_vehiculo = sequelize.define('det_usuario_vehiculo', {
-    "COD_USUARIO_VEHICULO": { type: DataTypes.INTEGER, primaryKey: true },
+    "COD_USUARIO_VEHICULO": { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     "RUT_USUARIO": { type: DataTypes.STRING },
     "PATENTE_COD_VEHICULO": { type: DataTypes.STRING }
-},
-    {
-        timestamps: false,
-        freezeTableName: true
-    });
+}, {
+    timestamps: false,
+    freezeTableName: true
+});
 
 Det_usuario_vehiculo.belongsTo(User, { foreignKey: 'RUT_USUARIO', as: 'user' });
 Det_usuario_vehiculo.belongsTo(Vehiculo, { foreignKey: 'PATENTE_COD_VEHICULO', as: 'vehiculo' });
