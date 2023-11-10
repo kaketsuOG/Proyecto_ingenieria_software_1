3a1b49727d7bfd29aed4fa85f9fa583d125092b4
NicolasPavz <npavez43@gmail.com>
2023-11-09 23:59:08 -0300

    producto

--- server/src/models/producto.ts
+++ server/src/models/producto.ts
@@ -1,14 +1,13 @@
 
 import {DataTypes } from 'sequelize';
 import sequelize from '../db/connection';
-import { Inventario } from './inventario';
 
 export const Producto = sequelize.define('producto',{
     "COD_PRODUCTO": {type: DataTypes.INTEGER,primaryKey:true,autoIncrement: true},
-    "COD_INVENTARIO": {type: DataTypes.INTEGER,},
+    "CANTIDAD_TOTAL": {type: DataTypes.INTEGER},
+    "CANTIDAD_DISPONIBLE": {type: DataTypes.INTEGER},
     "NOMBRE_PRODUCTO": {type: DataTypes.STRING},
     "PRECIO": {type: DataTypes.INTEGER}
-
 },
 {
     freezeTableName: true,
@@ -18,4 +17,3 @@ export const Producto = sequelize.define('producto',{
 
 }
 );
-Producto.belongsTo(Inventario,{ foreignKey: 'COD_INVENTARIO'});
