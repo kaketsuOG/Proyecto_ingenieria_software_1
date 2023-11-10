1115f696598a306e5b3a3148e6e9a2e300fbef74
GeoC <diego.cubilhos@gmail.com>
2023-11-10 00:02:46 -0300

    Eliminaciones

--- server/src/models/server.ts
+++ server/src/models/server.ts
@@ -1,6 +1,6 @@
-import express, {Application} from 'express';
+import express, { Application } from 'express';
 import cors from 'cors';
-import {Rol } from './rol';
+import { Rol } from './rol';
 import routesRoles from '../routes/roles';
 <<<<<<< HEAD
 import routesUser from '../routes/user';
@@ -15,10 +15,10 @@ import { Rol } from './rol';
 >>>>>>> 4500967e0e888fc8a1c5d49772043cbc1ee12f57
 import { User } from './user';
 import routesUser from '../routes/user';
-import {Sucursal} from './sucursal';
+import { Sucursal } from './sucursal';
 <<<<<<< HEAD
 import { Disponibilidad_fecha } from './dispo_fecha';
-import { Detalle_horario_entrega} from './det_horario_entrega'
+import { Detalle_horario_entrega } from './det_horario_entrega'
 =======
 import routesSucursal from '../routes/sucursal';
 import { Inventario } from './inventario';
@@ -42,9 +42,9 @@ import routesHistorial from '../routes/historial';
 
 class Server {
     private app: Application;
-    private port: string ;
+    private port: string;
 
-    constructor(){
+    constructor() {
         this.app = express();
         this.port = process.env.PORT || '3000';
 
@@ -54,32 +54,29 @@ class Server {
         this.dbConnect();
         this.routes();
 
-        
+
     }
 
-    listen(){
-        this.app.listen(this.port, ()=> {
+    listen() {
+        this.app.listen(this.port, () => {
             console.log('Corriendo en el puerto ' + this.port);
         })
     }
 
-    routes(){
+    routes() {
         this.app.use('/api/vehiculos', routesVehiculo);
-        this.app.use('/api/roles',routesRoles);
+        this.app.use('/api/roles', routesRoles);
         this.app.use('/api/users', routesUser);
-        this.app.use('/api/sucursal',routesSucursal);
-        this.app.use('/api/inventario',routesInventario);
         this.app.use('/api/productos', routesProducto);
 <<<<<<< HEAD
         this.app.use('/api/vehiculos', routesVehiculo);
-        this.app.use('/api/sucursal',routesSucursal);
-        this.app.use('/api/dispo_fechas',routesDispo_fecha);
-        this.app.use('/api/det_horario_entrega',routesDet_horario_entrega)
+        this.app.use('/api/dispo_fechas', routesDispo_fecha);
+        this.app.use('/api/det_horario_entrega', routesDet_horario_entrega)
 =======
         this.app.use('/api/cliente', routesCliente);
         this.app.use('/api/historial', routesHistorial);
-        
-        
+
+
 >>>>>>> 4500967e0e888fc8a1c5d49772043cbc1ee12f57
     }
 
@@ -89,28 +86,26 @@ class Server {
         this.app.use(cors());
     }
 
-    async dbConnect(){
-        try{
+    async dbConnect() {
+        try {
             await Vehiculo.sync()
             await Rol.sync()
             await User.sync()
-            await Sucursal.sync()
-            await Inventario.sync()
             await Producto.sync()
 <<<<<<< HEAD
             await Disponibilidad_fecha.sync()
             await Detalle_horario_entrega.sync()
-            
+
 =======
             await Cliente.sync()
             await Historial.sync()
 >>>>>>> 4500967e0e888fc8a1c5d49772043cbc1ee12f57
 
-            
-            
 
 
-        }catch (error){
+
+
+        } catch (error) {
             console.error('No se ha podido conectar a la base de datos');
         }
     }
