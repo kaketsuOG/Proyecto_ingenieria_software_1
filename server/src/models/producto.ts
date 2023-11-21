ef55d20776c1588bbf37c71e3ad2e769ecbabf19
GeoC <diego.cubilhos@gmail.com>
2023-11-20 23:17:45 -0300

    a

--- server/src/models/producto.ts
+++ server/src/models/producto.ts
@@ -2,16 +2,16 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../db/connection';
 
-export const Producto = sequelize.define('producto',{
-    "COD_PRODUCTO": {type: DataTypes.INTEGER,primaryKey:true,autoIncrement: true},
-    "CANTIDAD_TOTAL": {type: DataTypes.INTEGER},
-    "CANTIDAD_DISPONIBLE": {type: DataTypes.INTEGER},
-    "NOMBRE_PRODUCTO": {type: DataTypes.STRING},
-    "PRECIO_PRODUCTO": {type: DataTypes.INTEGER}
+export const Producto = sequelize.define('producto', {
+    "COD_PRODUCTO": { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    "NOMBRE_PRODUCTO": { type: DataTypes.STRING },
+    "PRECIO_PRODUCTO": { type: DataTypes.INTEGER },
+    "CANTIDAD_TOTAL": { type: DataTypes.INTEGER },
+    "CANTIDAD_DISPONIBLE": { type: DataTypes.INTEGER }
 },
     {
         freezeTableName: true,
         timestamps: false,
 
-}
+    }
 );
