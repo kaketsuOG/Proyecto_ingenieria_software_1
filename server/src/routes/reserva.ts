f87693b60cec2ed85511fe31ae720115dcce15f8
GeoC <diego.cubilhos@gmail.com>
2023-12-06 19:10:24 -0300

    /list

--- server/src/routes/reserva.ts
+++ server/src/routes/reserva.ts
@@ -5,18 +5,18 @@ import auth from './auth';
 const router = Router();
 
 // Endpoint para crear una nueva reserva
-router.post('/',newReserva);
+router.post('/', newReserva);
 
 // Endpoint para obtener la lista de reservas
-router.get('/',getReservas);
+router.get('/list', getReservas);
 
 // Endpoint para obtener una reserva por su código
-router.get('/:cod_reserva',getReserva);
+router.get('/:cod_reserva', getReserva);
 
 // Endpoint para actualizar una reserva por su código
-router.put('/:cod_reserva',updateReserva);
+router.put('/:cod_reserva', updateReserva);
 
 // Endpoint para eliminar una reserva por su código
-router.delete('/:cod_reserva',deleteReserva);
+router.delete('/:cod_reserva', deleteReserva);
 
 export default router;
