a775b1cbaf328970f9272ffea0c632b804cfd46e
kaketsuOG <seba.espinoza.f@gmail.com>
2023-12-13 07:54:09 -0300

    Final

--- server/dist/routes/detalle_reserva.js
+++ server/dist/routes/detalle_reserva.js
@@ -8,12 +8,12 @@ const detalle_reserva_1 = require("../controllers/detalle_reserva");
 const auth_1 = __importDefault(require("./auth"));
 const router = (0, express_1.Router)();
 // Obtener todos los detalles de reserva
-router.get('/list', auth_1.default, detalle_reserva_1.getDetallesReserva);
+router.get('/list', detalle_reserva_1.getDetallesReserva);
 // Obtener un detalle de reserva por ID
-router.get('/:cod_detalle_reserva', auth_1.default, detalle_reserva_1.getDetalleReserva);
+router.get('/:cod_detalle_reserva',  detalle_reserva_1.getDetalleReserva);
 // Crear un nuevo detalle de reserva
 // Actualizar un detalle de reserva por ID
-router.put('/:cod_detalle_reserva', auth_1.default, detalle_reserva_1.updateDetalleReserva);
+router.put('/:cod_detalle_reserva',  detalle_reserva_1.updateDetalleReserva);
 // Eliminar un detalle de reserva por ID
 router.delete('/:cod_detalle_reserva', auth_1.default, detalle_reserva_1.deleteDetalleReserva);
 exports.default = router;
